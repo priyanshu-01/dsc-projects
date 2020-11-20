8dd5a81e478c90b9ae533adeb844432758fb19c9
Utkarsh <chaudharyut25@gmail.com>
2020-11-20 22:50:07 +0530

    first commit

--- lib/pages/Dashboard/dashboard.dart
+++ lib/pages/Dashboard/dashboard.dart
@@ -4,7 +4,7 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey,
+      color: Colors.black,
       child: Center(
         child: Text('Dashboard page'),
       ),
